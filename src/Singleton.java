b42e8d4e83939023299f2730bd0120163ec152ce
Nutthapon Saefong <73591319+nut1245@users.noreply.github.com>
2021-09-06 16:08:59 +0700

    Initial commit

---
+++ src/Singleton.java
@@ -0,0 +1,17 @@
+public class Singleton {
+	private static Singleton uniqueInstance;
+	
+	private Singleton() {}
+	
+	public static  Singleton getInstance(){
+		if (uniqueInstance == null) {
+			uniqueInstance = new Singleton();
+		}
+		return  uniqueInstance;
+	}
+	
+	public String getDescription() {
+		return "I'm a Classic Singleton";
+	}
+	
+}
